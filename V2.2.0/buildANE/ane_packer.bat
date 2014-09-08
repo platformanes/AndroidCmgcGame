97238d1dbef369bdecbd1a76269d25164b704020
Rect <rectvv@gmail.com>
2014-09-08 20:29:12 +0800

    update v2

--- V2.2.0/buildANE/ane_packer.bat
+++ V2.2.0/buildANE/ane_packer.bat
@@ -1,1 +1,1 @@
-"D:\FlashBuilder\Adobe Flash Builder 4.6\sdks\AIR3.5\bin\adt" -package -storetype PKCS12 -keystore rect.p12 -storepass 1234 -target ane com.cmgc.ane extension.xml -swc *.swc -platform Android-ARM -C Android-ARM . 
+"D:\FlashBuilder\Adobe Flash Builder 4.6\sdks\AIR3.5\bin\adt" -package -tsa none -storetype PKCS12 -keystore rect.p12 -storepass 1234 -target ane com.cmgc.ane extension.xml -swc *.swc -platform Android-ARM -C Android-ARM . 
